67b9908273202e38ba16e01388af253732c6af69
KafeisM <jordifloritensenyat03@gmail.com>
2022-05-05 12:45:48 +0200

    Nombre

--- Entrega.java
+++ Entrega.java
@@ -26,9 +26,9 @@ import java.util.Set;
  *
  * Podeu fer aquesta entrega en grups de com a màxim 3 persones, i necessitareu com a minim Java 8.
  * Per entregar, posau a continuació els vostres noms i entregau únicament aquest fitxer.
- * - Nom 1:
- * - Nom 2:
- * - Nom 3:
+ * - Nom 1: Jordi Florit Ensenyat
+ * - Nom 2: Marc Ferrer Fernández
+ * - Nom 3: Raul Alcauter Sansó
  *
  * L'entrega es farà a través d'una tasca a l'Aula Digital abans de la data que se us hagui
  * comunicat i vos recomanam que treballeu amb un fork d'aquest repositori per seguir més fàcilment
